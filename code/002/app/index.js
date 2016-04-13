bd41d28ab13ae75fdc7156460769a6e47af76bdb
Tenchi <tenchi.wh.hsu@mail.foxconn.com>
2016-04-13 17:24:29 +0800

    更新002範例-accordion menu

--- code/002/app/index.js
+++ code/002/app/index.js
@@ -4,13 +4,18 @@ angular.module("myApp", [])
 
         return [
             {
-                title:'no1',
-                content:'no1-content'
+                title:'標題1',
+                content:'內文1'
 
             },
             {
-                title:'no2',
-                content:'no2-content'
+                title:'標題2',
+                content:'內文2'
+
+            },
+            {
+                title:'標題3',
+                content:'內文3'
 
             }
 
@@ -30,7 +35,28 @@ angular.module("myApp", [])
             restrict:'E',
             replace:true,
             template:'<div class="panel-group" ng-transclude></div>',
-            transclude:true
+            transclude:true,
+            controllerAs:'kittencupGroupController',
+            controller:function(){
+
+                this.groups = [];
+
+                this.closeOtherCollapse = function(nowScope)//nowScope =>目前被點選的項目
+                {
+
+                    angular.forEach(this.groups,function(scope)//循環每個子元素的元素
+                    {
+
+                        if(scope !== nowScope)//如果目前子元素不是被點選的項目，關閉
+                        {
+                            scope.isOpen = false;
+
+                        }
+                    })
+                }
+
+
+            }
 
 
         }
@@ -40,7 +66,32 @@ angular.module("myApp", [])
         return{
             restrict:'E',
             replace:true,
-            templateUrl:'app/tmp/kittencupCollapse.html'
+            require:'^kittencupGroup',//向上尋找元素(如果目標元素是父元素者)
+            templateUrl:'app/tmp/kittencupCollapse.html',
+            scope:{
+                'heading':'@'
+
+            },
+            link:function(scope,element,attrs,kittencupGroupController)//Link會把上一個controller(kittencupGroupController)自動依賴注入進來
+            {
+
+                console.log("kittencupGroupController");
+                console.log(kittencupGroupController);
+                scope.isOpen = false;//close
+                //scope.isOpen = false;//open
+
+                scope.onChangeOpen = function()
+                {
+                    scope.isOpen = !scope.isOpen;
+
+                    kittencupGroupController.closeOtherCollapse(scope);//關閉被點擊item以外的鄰居
+
+                }
+
+                kittencupGroupController.groups.push(scope);//存放子元素的每一個scope循環的value（isOpen）
+
+            },
+            transclude:true
 
         }
 
