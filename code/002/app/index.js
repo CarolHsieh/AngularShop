f63f7cb933093f39d30dde63dca28f529f0e8eee
Tenchi <tenchi.wh.hsu@mail.foxconn.com>
2016-04-13 14:05:30 +0800

    1.刪除git官方首頁的項目 2.新增002的範例

---
+++ code/002/app/index.js
@@ -0,0 +1,48 @@
+angular.module("myApp", [])
+    //數據
+    .factory('Data',function(){
+
+        return [
+            {
+                title:'no1',
+                content:'no1-content'
+
+            },
+            {
+                title:'no2',
+                content:'no2-content'
+
+            }
+
+        ]
+    })
+    //控制器
+    .controller('firstController',['$scope','Data',function($scope,Data){
+
+        $scope.data = Data;
+
+
+    }])
+    .directive('kittencupGroup',function(){
+
+
+        return{
+            restrict:'E',
+            replace:true,
+            template:'<div class="panel-group" ng-transclude></div>',
+            transclude:true
+
+
+        }
+    })
+    .directive('kittencupCollapse',function(){
+
+        return{
+            restrict:'E',
+            replace:true,
+            templateUrl:'app/tmp/kittencupCollapse.html'
+
+        }
+
+    })
+
